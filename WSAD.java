7fa74c7675beadc8e1816c46e6ea98a17f5a64ad
Yomal <yomal.praveen@gmail.com>
2017-10-28 13:35:22 +0530

    roundone

--- WSAD.java
+++ WSAD.java
@@ -11,52 +11,77 @@ import robocode.*;
  */
 public class WSAD extends AlphaBot{
    
+    boolean peek; // Don't turn if there's a robot there
+	double moveAmount; // How much to move
+    
    public void run()
    {
-       while(true)
-       {
-           //ahead(300);
-           turnRadarLeft(360);
-           //ahead(100);
-       }
+       
+       moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		// Initialize peek to false
+        peek = false;
+
+        turnLeft(getHeading() % 90);
+        ahead(moveAmount);
+        // Turn the gun to turn right 90 degrees.
+        peek = true;
+        turnGunRight(90);
+        turnRight(90);
+
+        while (true) {
+                // Look before we turn when ahead() completes.
+                peek = true;
+                // Move up the wall
+                ahead(moveAmount);
+                // Don't look now
+                peek = false;
+                // Turn to the next wall
+                turnRight(90);
+        }
+
    }
 
+    public void move()
+    {
+        
+    }
+   
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {   
         
-        double angle = getGunHeading();
+        double objHeading;
+        double gunHeading = getGunHeading();
+        objHeading = event.getBearing() + getHeading();
         
-        event.getBearing();
+        if(objHeading > 360)
+            objHeading -= 360;
         
-        /*
-        if(event.getBearing() > 0){
-            turnGunRight(event.getBearing());
-            turnRadarRight(event.getBearing());
+        double enemybearing = objHeading - gunHeading;
+        
+        if(enemybearing == 0)
+        {
+            fire(1);
+        }
+        else if(enemybearing < 180 )
+        {
+            turnGunRight(objHeading - gunHeading);
+            fire(1);
         }
         else
         {
-            turnGunLeft(-event.getBearing());
-            turnRadarLeft(-event.getBearing());
+            turnGunLeft(objHeading - gunHeading);
+            fire(1);
         }
-        //fire(3);
-        */
+        
     }
-  
-
+    
     @Override
     public void onHitByBullet(HitByBulletEvent event) {
         turnLeft(90);
-        back(250);
         ahead(300);
     }
 
-    @Override
-    public void onHitWall(HitWallEvent event) {
-        turnRight(event.getBearing() + 90);
-        ahead(300);
-        back(200);
-        turnGunRight(90);
-    }
+
    
     
    
